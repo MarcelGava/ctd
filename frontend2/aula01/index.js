6b11faeee0e2b304c3e2c04c99f578222d044958
MARCEL <marcel.salles16@gmail.com>
2022-02-22 10:14:56 -0300

    Adicionado repositório de Front End 2 e aual 01

---
+++ frontend2/aula01/index.js
@@ -0,0 +1,14 @@
+let nome = "Marcel Gava";
+
+var idade = 28 
+console.log("Olá, eu sou "+nome+" e tenho "+idade+" anos");
+
+
+
+let numeros = [1,2,3,4,5]
+let resultado = 0
+for (let i = 0; i < numeros.length; i++)  {
+    resultado += numeros[i]
+    console.log(resultado)
+}
+ 
