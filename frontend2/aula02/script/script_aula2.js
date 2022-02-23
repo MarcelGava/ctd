f4cfce17ce97a3a18e5663ecbe0b958f0b010e58
MARCEL <marcel.salles16@gmail.com>
2022-02-23 07:23:46 -0300

    Adicionados os arquvivos da aula 02

---
+++ frontend2/aula02/script/script_aula2.js
@@ -0,0 +1,34 @@
+//captura  e exibição de informações
+
+// let usuario = "Marcel";
+
+// // alert("Usuário " + usuario + " logado com sucesso"); 
+
+// let respostaConfirm;
+
+// respostaConfirm = confirm("Você realmente deseja continaur a sessão?");
+
+// console.log(respostaConfirm)
+
+
+// if(respostaConfirm) {
+//     alert("Seja bem-vindo!")
+// }else {
+//     alert("Volte mais tarde.")
+// }10
+
+
+// formas de converter dados em JS
+// let valor1 = 10;
+// let valor; 
+
+// valor = prompt("Insiera um número inteiro");
+
+// let valorConvertido = parseFloat(valor)
+
+// let resultado = valorConvertido + valor1;
+
+// alert(resultado)
+
+let numeroAle= Math.random();
+alert(numeroAle)
